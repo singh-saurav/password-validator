37213aa680201aad00b62accffca84d3a5d8dcfd
singh-saurav <saurav.singh@lns.maersk.com>
2022-10-27 18:53:24 +0530

    timeout if test exceeded 1 sec

--- src/test/java/com/maersk/test/PasswordCheckerTest.java
+++ src/test/java/com/maersk/test/PasswordCheckerTest.java
@@ -1,8 +1,11 @@
 package com.maersk.test;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,6 +13,7 @@ class PasswordCheckerTest {
 
     @DisplayName("OK password matching all or special conditions.")
     @ParameterizedTest
+    @Timeout(value = 1)
     @ValueSource(strings = {"Password123", "passwd1"})
     void checkOk(String password) {
         assertEquals("OK", PasswordChecker.check(password));
@@ -17,6 +21,7 @@ class PasswordCheckerTest {
 
     @DisplayName("NOT_OK password not matching all or special conditions.")
     @ParameterizedTest
+    @Timeout(value = 1)
     @ValueSource(strings = {"", "passwd", "SAURAV"})
     void checkInValid(String password) {
         assertEquals("NOT_OK", PasswordChecker.check(password));
